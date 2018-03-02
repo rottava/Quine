33e72fbb19bbe9cb9c6c578cdc715769b5f2aca1
Júnior Rottava <juniorrottava@hotmail.com>
2018-03-02 17:16:41 -0300

    V.2

--- main.c
+++ main.c
@@ -1,141 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned short int init();
-char **criaMatriz(unsigned short int colunas, unsigned int linhas);
-char *mintermo(unsigned short int linha, unsigned short int colunas, unsigned int linhas);
-void printMatriz(unsigned short int colunas, unsigned int linhas, char** matriz);
-void associaMintermos(unsigned short int colunas, unsigned int *linhas, char **matriz);
-void printResult(unsigned short int colunas, unsigned int linhas, char **matriz);
-void copiaMatriz(unsigned short int colunas, unsigned short int linhasA, unsigned short int linhasB, char **matriz, char **MatrizAux);
+struct Data{
+    unsigned char *minte;
+    unsigned int *cober;
+    unsigned int taman;
+};
+
+struct Data *matriz;
+unsigned int linhas;
+unsigned int colunas;
+
+void mintermo(int pos, int linha);
+void printMatriz();
+void printResult();
+void associaMintermos();
 
 int main(){
-    unsigned short int colunas;
-    unsigned int aux1, aux2, linhas, sdp;
-    char **matriz, **matrizAux, *mintermos;
+    int aux, i, j, mask;
 
     colunas = 1;
+    linhas = 0;
 
-    while(colunas > 0){
-        colunas = init();                                               //A,B,C,D...
-        aux2 = 0;
-        linhas = 2 << (colunas - 1);                                    //2^Numero de variaveis
-        sdp = 0;
-        matriz = criaMatriz(colunas, linhas);
-        printf("Insira as linhas com valor 1 da tabela verdade.\nInsira 0 para encerrar.\n");
+    while(colunas != 0){
+        printf("Para sair digite 0\nNumero de variaveis: ");
+        scanf("%u", &colunas);
 
-        while (aux2 == 0){                                              //Le numero da linha soma de produto
-            scanf("%d", &aux1);
-
-            if(aux1 == 0){
-                aux2++;
+        if(colunas != 0){
+            printf("Numero de linhas: ");
+            scanf("%u", &linhas);
+            matriz = calloc(2<<colunas, sizeof(struct Data));
+            if(matriz == NULL) {
+                printf("Erro, Memoria insuficiente para a tabela verdade!\n");
+                return -1;
             }
-
-            else if(aux1 > linhas){
-                printf("Essa linha nao existe na tabela!\n");
-            }
-
-            else{
-                mintermos = mintermo(aux1, colunas, linhas);            //Gera mintermo apartir da tabela verdade
-                for(aux1 = 0; aux1 < colunas; aux1++){
-                    matriz[sdp][aux1] = mintermos[aux1];                //Salva mintermo na matriz
+            for(i = 0; i < 2<<colunas; i++){
+                matriz[i].minte = calloc(colunas, sizeof(char));
+                if(matriz[i].minte == NULL) {
+                    printf("Erro, Memoria insuficiente para a tabela verdade!\n");
+                    return -1;
                 }
-                free(mintermos);
-                sdp++;
+                matriz[i].cober = calloc(linhas, sizeof(int));
+                if(matriz[i].cober == NULL) {
+                    printf("Erro, Memoria insuficiente para a tabela verdade!\n");
+                    return -1;
+                }
+                matriz[i].taman = 0;
             }
-        }
+            for(i = 0; i < linhas; i++){
+                printf("Linha da tabela verdade: ");
+                scanf("%d", &aux);
 
-        if(sdp < linhas){                                               //Realoca o tamanho da matriz caso use menos espaço que o esperado
-            matrizAux = criaMatriz(colunas, sdp);
-            copiaMatriz(colunas, sdp, linhas, matriz, matrizAux);
-        }
+                for(j = 0; j < colunas; j++){
+                    mask = aux >> (colunas-1-j);
+                    if(mask & 1){
+                       matriz[i].minte[j] = '1';
+                    }
 
-        printf("\nMatriz de entrada\n");
-        printMatriz(colunas, sdp, matriz);                              //Imprime tabela de entrada
-        associaMintermos(colunas, &sdp, matriz);
-        printf("\nMatriz de saida\n");
-        printMatriz(colunas, sdp, matriz);                              //Imprime tabela de entrada
-        printResult(colunas, sdp, matriz);
+                    else{
+                        matriz[i].minte[j] = '0';
+                    }
+                }
+
+                matriz[i].cober[0] = aux;
+                matriz[i].taman = 1;
+            }
+            printf("\nTabela de entrada: \n");
+            printMatriz();
+            associaMintermos();
+            printf("\nTabela de saida: \n");
+            printMatriz();
+            printf("\nFuncao minimizada: ");
+            printResult();
+        }
     }
 
     return 0;
 }
 
-unsigned short int init(){
-    unsigned int colunas;
-
-    colunas = 27;
-
-    while(colunas > 26){
-        printf("Insira o numero de variaveis.\nInsira 0 para encerrar.\n");
-        scanf("%d", &colunas);
-
-        if(colunas > 26){
-            printf("Nao pode-se calcular com mais de 26 variaveis, tente novamente.\n");
-        }
-
-        else if(colunas == 0){
-            printf("Saindo do programa...");
-            exit(0);
-        }
-
-    }
-
-    return colunas;
-}
-
-char **criaMatriz(unsigned short int colunas, unsigned int linhas){
-    char **matriz;
-    unsigned int i, j;
-
-	matriz = calloc(linhas, sizeof(char*));               //Aloca uma matriz na memoria
-
-	if(matriz == NULL){
-		printf("Erro, Memoria insuficiente para a tabela verdade!\n");
-		exit(-1);
-	}
-
-	else{
-		for(i = 0; i < linhas; i++){
-			matriz[i] = calloc(colunas, sizeof(char));      //Aloca um vetor de caracteres para cada posição do vetor de ponteiros.
-
-			if(matriz[i] == NULL){
-				printf("Erro, Memoria insuficiente para tabela verdade!\n");
-				exit(-1);
-			}
-
-			else{
-				for (j = 0; j < colunas; j++){
-					matriz[i][j] = 0; 							    //Inicializa cada posição do vetor de caracteres com 0.
-                }
-			}
-		}
-	}
-
-	return matriz;
-}
-
-char *mintermo(unsigned short int linha, unsigned short int colunas, unsigned int linhas){
-    char *mint;
-    int i, mask;
-
-    mint = calloc(colunas, sizeof(char));
-
-    for(i = 0; i < colunas; i++){
-        mask = linhas >> (i+1);
-        if((linha-1) & mask){
-           mint[i] = '1';
-        }
-
-        else{
-            mint[i] = '0';
-        }
-    }
-
-    return mint;
-}
-
-void printMatriz(unsigned short int colunas, unsigned int linhas, char** matriz){
+void printMatriz(){
     unsigned int i, j;
 
     j = 'A';
@@ -145,114 +88,29 @@ void printMatriz(unsigned short int colunas, unsigned int linhas, char** matriz)
         j++;
     }
 
-    printf("|SDP\n");
-
-    for(i = 0; i < linhas; i++) {
-        for(j = 0; j < colunas; j++) {
-            printf("%c ", matriz[i][j]);
-        }
-
-        printf("| 1\n");
-    }
-}
-
-void associaMintermos(unsigned short int colunas, unsigned int *linha, char **matriz){
-    int aux1, aux2, i, j, k, linhas, linhasRe, pass;
-    char **matrizAux;
-
-    linhas = *linha;
-    matrizAux = criaMatriz(colunas, linhas);
-    linhasRe = 0;
-
-    while(pass != 0){
-        pass = 0;
-
-        for(i = 0; i < linhas; i++){                                //Copia a matriz
-            for(j = 0; j < colunas; j++){
-                matrizAux[i][j] = matriz[i][j];
-            }
-        }
-
-        for(i = 0; i < linhas; i++){
-            for(j = (i+1); j < linhas; j++){
-                aux1 = 0;
-
-                for(k = 0; k < colunas; k++){
-                    if(matrizAux[i][k] == matrizAux[j][k]){
-                        aux1++;
-                    }
-
-                    else{
-                        aux2 = k;
-                    }
-                }
-
-                if(aux1 == colunas-1 && matriz[i][0] != '.'){
-                    for(aux1 = 0; aux1 < colunas; aux1++){
-                        matriz[i][aux1] = '.';
-                    }
-                    matriz[j][aux2] = '-';
-                    linhasRe++;
-                    pass++;
-                }
-
-                else if(aux1 == colunas && matrizAux[i][0] != '.'){
-                    for(aux1 = 0; aux1 < colunas; aux1++){
-                        matriz[i][aux1] = '.';
-                    }
-                    linhasRe++;
-                }
-            }
-        }
-    }
-
-    for(i = 0; i < linhas; i++){
-        free(matrizAux[i]);
-    }
-
-    free(matrizAux);
-    linhasRe = linhas - linhasRe;
-    matrizAux = criaMatriz(colunas, linhasRe);
-    aux1 = 0;
-
-    for(i = 0; i < linhas; i++){
-        if(matriz[i][0] != '.'){
-            for(j = 0; j < colunas; j++){
-                matrizAux[aux1][j] = matriz[i][j];
-            }
-            aux1++;
-        }
-
-        free(matriz[i]);
-    }
-
-    free(matriz);
-    matriz = criaMatriz(colunas, linhasRe);
-
-    for(i = 0; i < linhasRe; i++){
-        for(j = 0; j < colunas; j++){
-            matriz[i][j] = matrizAux[i][j];
-        }
-
-        free(matrizAux[i]);
-    }
-
-    free(matrizAux);
-    *linha = linhasRe;
-}
-
-void printResult(unsigned short int colunas, unsigned int linhas, char** matriz){
-    unsigned int i, j;
-
-    j = 'A';
     printf("\n");
 
     for(i = 0; i < linhas; i++) {
         for(j = 0; j < colunas; j++) {
-            if(matriz[i][j] == '0'){
-                printf("%c'", 'A'+j);
+            printf("%c ", matriz[i].minte[j]);
+        }
+
+        printf("\n");
+    }
+}
+
+void printResult(){
+    unsigned int i, j;
+
+    printf("\n");
+
+    for(i = 0; i < linhas; i++) {
+        for(j = 0; j < colunas; j++) {
+            if(matriz[i].minte[j] == '0'){
+                printf("!%c", 'A'+j);
+
             }
-            else if(matriz[i][j] == '1'){
+            if(matriz[i].minte[j] == '1'){
                 printf("%c", 'A'+j);
             }
         }
@@ -264,36 +122,193 @@ void printResult(unsigned short int colunas, unsigned int linhas, char** matriz)
     printf("\n\n");
 }
 
-void copiaMatriz(unsigned short int colunas, unsigned short int linhasA, unsigned short int linhasB, char **matriz, char **matrizAux){
-    int aux, i, j;
+void associaMintermos(){
+    int aux1, aux2, *cobertura, i, j, k, l, ok, pass, temp, val;
+    struct Data *matrizAux;
+    char *matrizVal, *nova;
 
-    aux = 0;
-
-    for(i = 0; i < linhasA; i++){
-        if(matriz[i][0] != '.'){
-            for(j = 0; j < colunas; j++){
-                matrizAux[aux][j] = matriz[i][j];
-            }
-            aux++;
-        }
-
-        free(matriz[i]);
+    nova = calloc(colunas, sizeof(char));
+    if(nova == NULL){
+        printf("Erro, Memoria insuficiente para a tabela verdade!\n");
+		exit(-1);
     }
 
-    for(i = linhasA; i < linhasB; i++){
-        free(matriz[i]);
+    matrizAux = calloc(2<<colunas, sizeof(struct Data));
+    if(matrizAux == NULL){
+        printf("Erro, Memoria insuficiente para a tabela verdade!\n");
+		exit(-1);
+    }
+    temp = 0;
+    val = 0;
+    matrizVal = calloc(2<<colunas, sizeof(int));
+
+    if(matrizVal == NULL){
+        printf("Erro, Memoria insuficiente para a tabela verdade!\n");
+		exit(-1);
     }
 
-    free(matriz);
-    matriz = criaMatriz(colunas, linhasA);
-
-    for(i = 0; i < linhasA; i++){
+    for(i = 0; i < 2<<colunas; i++){
+        matrizAux[i].minte = calloc(colunas, sizeof(char));
         for(j = 0; j < colunas; j++){
-            matriz[i][j] = matrizAux[i][j];
+            matrizAux[i].minte[j] = '.';
         }
-
-        free(matrizAux[i]);
+        matrizAux[i].cober = calloc(linhas, sizeof(int));
+        if(matrizAux[i].cober == NULL) {
+            printf("Erro, Memoria insuficiente para a tabela verdade!\n");
+            exit(-1);
+        }
+        matrizAux[i].taman = 0;
     }
 
-    free(matrizAux);
+    for(i = 0; i < 2<<colunas; i++){
+        matrizVal[i] = '0';
+    }
+
+    while(pass != 0){
+        pass = 0;
+        //Minimiza tabela
+        for(i = 0; i < linhas; i++){
+            for(j = (i+1); j < linhas; j++){
+                aux1 = 0;
+
+                for(k = 0; k < colunas; k++){
+                    if(matriz[i].minte[k] == matriz[j].minte[k]){
+                        aux1++;
+                    }
+                    else{
+                        aux2 = k;
+                    }
+                }
+                printf("%s | %s\n", matriz[i].minte, matriz[j].minte);
+                if(aux1 == colunas-1){
+                    matrizVal[i] = '1';
+                    matrizVal[j] = '1';
+                    ok = 0;
+                    for(k = 0; k < colunas; k++){
+                        nova[k] = matriz[j].minte[k];
+                    }
+                    nova[aux2] = '-';
+                    //Verifica se ja existe na matriz
+                    for(k = 0; k < val; k++){
+                        aux2 = 0;
+                        for(l = 0; l < colunas; l++){
+                            if(nova[l] == matrizAux[k].minte[l]){
+                                aux2++;
+                            }
+                        }
+                        if(aux2 == colunas){
+                            k = val;
+                            ok = 1;
+                        }
+                    }
+                    //Se nao existe
+                    if (ok == 0){
+                        for(k = 0; k < colunas; k++){
+                            matrizAux[val].minte[k] = nova[k];
+                        }
+                        for(k = 0; k < matriz[i].taman; k++){
+                            matrizAux[val].cober[k] = matriz[i].cober[k];
+                        }
+                        for(k = 0; k < matriz[j].taman; k++){
+                            matrizAux[val].cober[matriz[i].taman + k] = matriz[j].cober[k];
+                        }
+                        matrizAux[val].taman = matriz[i].taman + matriz[j].taman;
+                        val++;
+                        pass++;
+                        printf("Sai : %s | %s\n", matriz[i].minte, matriz[j].minte);
+                        printf("Nova: %s\n", nova);
+                    }
+                }
+            }
+        }
+
+        //Copia valores nÃ£o reduzidos para matriz auxiliar
+        for(i = 0; i < linhas; i++){
+            if(matrizVal[i] == '0'){
+                for(j = 0; j < colunas; j++){
+                    matrizAux[val].minte[j] = matriz[i].minte[j];
+                }
+                for(j = 0; j < matriz[i].taman; j++){
+                    matrizAux[val].cober[j] = matriz[i].cober[j];
+                }
+                matrizAux[val].taman = matriz[i].taman;
+                val++;
+            }
+        }
+
+        linhas = val;
+
+        if(temp != val){
+            temp = val;
+            //Prepara para nova etapa
+            for(i = 0; i < linhas; i++){
+                //Copia da matriz auxiliar para matriz original as minimizacoes
+                for(j = 0; j < colunas; j++){
+                    matriz[i].minte[j] = matrizAux[i].minte[j];
+                }
+                for(j = 0; j < matrizAux[i].taman; j++){
+                    matriz[i].cober[j] = matrizAux[i].cober[j];
+                }
+                matriz[i].taman = matrizAux[i].taman;
+                matrizVal[i] = '0';
+            }
+            val = 0;
+            pass++;
+        }
+    }
+    ///HERE
+
+    //Descobre linhas da cobertura e aloca vetor de acordo com tamanho
+    aux1 = 0;
+    for(i = 0; i < linhas; i++){
+        aux1 += matrizAux[i].taman;
+    }
+    cobertura = calloc(aux1, sizeof(int));
+    if(cobertura == NULL){
+        printf("Erro, Memoria insuficiente para a tabela verdade!\n");
+        exit(-1);
+    }
+    //Verifica cobertura
+    for(i = 0; i < linhas; i++){
+        aux1 = 0;
+        aux2 = 0;
+        //Cria cobertura sem mintermo atual
+        for(j = 0; j < linhas; j++){
+            if(i != j){
+                for(k = 0; k < matrizAux[j].taman; k++){
+                    cobertura[aux1] = matrizAux[j].cober[k];
+                    aux1++;
+                }
+            }
+        }
+        for(j = 0; j < matrizAux[i].taman; j++){
+            for(k = 0; k < aux1; k++){
+                if(matrizAux[i].cober[j] == cobertura[k]){
+                    aux2++;
+                    k = aux1;
+                }
+            }
+        }
+        //Significa que todos ja estao cobertos por outros
+        if(aux2 == matrizAux[i].taman){
+            for(j = i; j < linhas-1; j++){
+                matrizAux[j].cober = matrizAux[j+1].cober;
+                matrizAux[j].minte = matrizAux[j+1].minte;
+                matrizAux[j].taman = matrizAux[j+1].taman;
+            }
+            linhas--;
+            i--;
+        }
+    }
+
+    //Copia para matriz de retorno
+    for(i = 0; i < linhas; i++){
+        for(j = 0; j < colunas; j++){
+            matriz[i].minte[j] = matrizAux[i].minte[j];
+        }
+        for(j = 0; j < matrizAux[i].taman; j++){
+            matriz[i].cober[j] = matrizAux[i].cober[j];
+        }
+        matriz[i].taman = matrizAux[i].taman;
+    }
 }
